81d2324cf5acc3c59af3fc29da8f26fcce7bcced
hatuhn <hatuhn@hatuhns-MacBook-Pro.local>
2019-09-16 16:54:47 +0700

    Upate

--- templates/single-employer/parts/map.php
+++ templates/single-employer/parts/map.php
@@ -7,8 +7,10 @@
 <div class="opaljob-panel panel-section-map"  id="employer-map">
 	<div class="panel-heading"><h4><?php esc_html_e( 'Address', 'opaljob' ); ?></h4></div>
 	<div class="panel-body">
-	    <div  id="job-map<?php echo esc_attr($id); ?>" class="job-preview-map"  style="height:500px" data-latitude="<?php echo( isset( $maps['latitude'] ) ? $maps['latitude'] : '' ); ?>"
-                             data-longitude="<?php echo( isset( $maps['longitude'] ) ? $maps['longitude'] : '' ); ?>" data-icon="<?php echo esc_url( OPALJOB_CLUSTER_ICON_URL ); ?>"></div>
+	    <div  id="job-map<?php echo esc_attr($id); ?>" class="job-preview-map"  style="height:500px" 
+	    	data-latitude="<?php echo( isset( $maps['latitude'] ) ? $maps['latitude'] : '' ); ?>"
+            data-longitude="<?php echo( isset( $maps['longitude'] ) ? $maps['longitude'] : '' ); ?>" 
+            data-icon="<?php echo esc_url( OPALJOB_CLUSTER_ICON_URL ); ?>"></div>
 
 	</div>
 </div>
