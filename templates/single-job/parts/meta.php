69f3a5209aa4ecd875daa5e3e61117ce70fe51d2
hatuhn <hatuhn@hatuhns-MacBook-Pro.local>
2019-09-17 08:51:24 +0700

    uapte

--- templates/single-job/parts/meta.php
+++ templates/single-job/parts/meta.php
@@ -1,10 +1,10 @@
 <div class="job-meta-list"><ul class="meta-list-inner">
 	<?php foreach( $metas as $meta ): ?>
-		<li class="meta-item">
-			<div class="meta-icon">
+		<li class="meta-item d-flex">
+			<div class="meta-icon icon-md">
 				<i class="<?php echo $meta['icon'];?>"></i>
 			</div>	
-			<div class="meta-content">
+			<div class="meta-content ml-3">
 				<span class="meta-label"><?php echo $meta['label']; ?></span>
 				<span class="meta-text"><?php echo $meta['content']; ?></span>
 			</div>
